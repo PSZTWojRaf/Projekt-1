bfe4186cd65f7ef2ba86bee56d0d04d779adcca8
Rafanik <rafalszczepanik1998@gmail.com>
2020-04-01 16:28:29 +0200

    Kilka zmian poprawiających efektywność. Sortowanie jeszcze nieusunięte.

--- TestyMatLab/Astar.m
+++ TestyMatLab/Astar.m
@@ -1,31 +1,36 @@
 function [PATH, D] = Astar(G,start,goal, heuristic)
 
+start_idx = findnode(G,start);
+goal_idx = findnode(G,goal);
+
 % funkcja heurystyczna
     function h = h(idxA)
         if heuristic == false
             h = 0;
         else
-            h = sqrt((G.Nodes.X(findnode(G,goal))*ones(length(idxA),1) - G.Nodes.X(idxA)).^2 + (G.Nodes.Y(findnode(G,goal))*ones(length(idxA),1) - G.Nodes.Y(idxA)).^2);
+            h = sqrt((G.Nodes.X(goal_idx)*ones(length(idxA),1) - G.Nodes.X(idxA)).^2 + (G.Nodes.Y(goal_idx)*ones(length(idxA),1) - G.Nodes.Y(idxA)).^2);
         end
     end
 
 
+
+
 % zbiór otwarty
-OpenSet = zeros(length(neighbors(G,findnode(G,start))),3);
-OpenSet(:,1) = neighbors(G,findnode(G,start));
+OpenSet = zeros(length(neighbors(G,start_idx)),3);
+OpenSet(:,1) = neighbors(G,start_idx);
 
 % przygotowanie wektorów do znalezienia krawêdzi
-S = cell(length(OpenSet), 1);
-S(:,1) = {start};
-T = cell(length(OpenSet), 1);
-T(:,1) = G.Nodes.Name(OpenSet(:,1));
+S = ones(length(OpenSet),1)*start_idx;
+T = OpenSet(:,1);
 
 % za³adowanie pierwszego kroku algorytmu
 % rozwiniêcie stanu
-OpenSet(:,3) = G.Edges.Weight(findedge(G,S,T)) + h(findnode(G,T));
 OpenSet(:,2) = G.Edges.Weight(findedge(G,S,T));
+OpenSet(:,3) = OpenSet(:,2) + h(OpenSet(:,1));
+
 % posortowanie zbioru stanów otwartych
-OpenSet = sortrows(OpenSet,2);
+min_idx
+OpenSet = sortrows(OpenSet,3);
 % dodanie kolumny na stany poprzedzaj¹ce
 OpenSet(:,4) = zeros(length(OpenSet(:,1)),1);
 
@@ -50,13 +55,16 @@ while size(OpenSet) ~= size([])
                 OpenSet(:,end+1) = zeros(length(OpenSet(:,1)),1);
             end
             
-            % dodanie wêz³ów do zbioru stanów otwartych
-            OpenSet(end+1,1:3) = [temp(i), OpenSet(1,2) + G.Edges.Weight(findedge(G,OpenSet(1,1),temp(i))) , OpenSet(1,2) + G.Edges.Weight(findedge(G,OpenSet(1,1),temp(i)))+h(temp(i))];
+            % dodanie wêz³ów do zbioru stanów otwartych wraz z d³ugoœciami
+            % œcie¿ek
+            OpenSet(end+1,1) = temp(i);
+            OpenSet(end,2) = OpenSet(1,2) + G.Edges.Weight(findedge(G,OpenSet(1,1),temp(i)));
+            OpenSet(end,3) = OpenSet(end,2)+h(temp(i));
             % dodanie wêz³a poprzedzaj¹cego
             OpenSet(end,4) = OpenSet(1,1);
             % przepisanie wêz³ów poprzedzaj¹cych wêze³ poprzedzaj¹cy
             OpenSet(end,5:end) = OpenSet(1,4:end-1);
-        end
+       end
     end
     
     % usuniêcie wêz³a, który by³ rozwijany
@@ -66,9 +74,9 @@ while size(OpenSet) ~= size([])
     
     % posortowanie zbioru stanów otwartych
     OpenSet = sortrows(OpenSet,3);
-    
+    %[val min_idx] = min(OpenSet(:,3));
     % sprawdzenie czy wêze³ na szczycie jest wêz³em koñcowym
-    if strcmp(G.Nodes.Name(OpenSet(1,1)), goal)
+    if OpenSet(1,1) == goal_idx
         % jeœli to prawda, zwrócenie œcie¿ki i d³ugoœci œcie¿ki
         TEST = OpenSet(1,OpenSet(1,:)~=0);
         START = cellstr(start);
