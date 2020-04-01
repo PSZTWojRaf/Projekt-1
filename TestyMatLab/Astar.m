f687c5706d5460e5748e157ac0b0b9a140b53728
Rafanik <rafalszczepanik1998@gmail.com>
2020-04-01 17:13:58 +0200

    Wersja operująca na wektorach i macierzach.

--- TestyMatLab/Astar.m
+++ TestyMatLab/Astar.m
@@ -1,12 +1,19 @@
 function [PATH, D] = Astar(G,start,goal, heuristic)
 
+% G - przeszukiwany graf
+% start - nazwa wêz³a poczatkowego
+% goal - nazwa wêz³a koñcowego
+% heuristic (boolean) - czy u¿ywaæ funkcji h
+% PATH - najkrótsza œcie¿ka
+% D - d³ugoœæ najkrótszej œcie¿ki
+
 start_idx = findnode(G,start);
 goal_idx = findnode(G,goal);
 
 % funkcja heurystyczna
     function h = h(idxA)
         if heuristic == false
-            h = 0;
+            h = zeros(length(idxA),1);
         else
             h = sqrt((G.Nodes.X(goal_idx)*ones(length(idxA),1) - G.Nodes.X(idxA)).^2 + (G.Nodes.Y(goal_idx)*ones(length(idxA),1) - G.Nodes.Y(idxA)).^2);
         end
@@ -29,74 +36,75 @@ OpenSet(:,2) = G.Edges.Weight(findedge(G,S,T));
 OpenSet(:,3) = OpenSet(:,2) + h(OpenSet(:,1));
 
 % posortowanie zbioru stanów otwartych
-min_idx
-OpenSet = sortrows(OpenSet,3);
+
+[~, min_idx] = min(OpenSet(:,3));
 % dodanie kolumny na stany poprzedzaj¹ce
 OpenSet(:,4) = zeros(length(OpenSet(:,1)),1);
 
 % zbiór stanów zamkniêtych ???
 ClosedSet(1,1) = [0];
 
-
-
 %for k =1:5
 k = 0;
 while size(OpenSet) ~= size([])
+    % iterator do debuggu
     k = k+1;
+    
     % rozwiniêcie wêz³a o najtañszej œcie¿ce dotychczasowej
-    temp = neighbors(G,OpenSet(1,1));
-    % sprawdzenie czy s¹ w zbiorze wêz³ów zamkniêtych
-    for i = 1:length(temp)
-        % sprawdzenie czy jest w stanach zamkniêtych ???
-        if sum(ClosedSet(:,1) == temp(i)) == 0
-            
-            % dodanie kolumny na poprzednie wêz³y, jeœli jest konieczna
-            if OpenSet(1,end)~=0
-                OpenSet(:,end+1) = zeros(length(OpenSet(:,1)),1);
-            end
-            
-            % dodanie wêz³ów do zbioru stanów otwartych wraz z d³ugoœciami
-            % œcie¿ek
-            OpenSet(end+1,1) = temp(i);
-            OpenSet(end,2) = OpenSet(1,2) + G.Edges.Weight(findedge(G,OpenSet(1,1),temp(i)));
-            OpenSet(end,3) = OpenSet(end,2)+h(temp(i));
-            % dodanie wêz³a poprzedzaj¹cego
-            OpenSet(end,4) = OpenSet(1,1);
-            % przepisanie wêz³ów poprzedzaj¹cych wêze³ poprzedzaj¹cy
-            OpenSet(end,5:end) = OpenSet(1,4:end-1);
-       end
+    NewNodes = neighbors(G,OpenSet(min_idx,1));
+    
+
+    
+    NewNodes_length = length(NewNodes);
+    % dodanie kolumny na poprzednie wêz³y, jeœli jest konieczna
+    if OpenSet(min_idx,end)~=0
+        OpenSet(:,end+1) = zeros(length(OpenSet(:,1)),1);
     end
     
-    % usuniêcie wêz³a, który by³ rozwijany
-    % G.Nodes.Name(OpenSet(1,[true false false OpenSet(1,4:end)~=0]))
-    OpenSet(1,:) = [];
-    ClosedSet(end+1,1) = OpenSet(1,1);
+    % dodanie indeksów rozwiniêtych wêz³ów do przestrzeni przeszukiwañ
+    OpenSet(end+1:end+NewNodes_length,1) = NewNodes;
     
-    % posortowanie zbioru stanów otwartych
-    OpenSet = sortrows(OpenSet,3);
-    %[val min_idx] = min(OpenSet(:,3));
-    % sprawdzenie czy wêze³ na szczycie jest wêz³em koñcowym
-    if OpenSet(1,1) == goal_idx
+    % dodanie informacji o d³ugoœci œcie¿ki
+    OpenSet(end-NewNodes_length+1:end,2) = ones(NewNodes_length,1)*OpenSet(min_idx,2) + G.Edges.Weight(findedge(G,ones(NewNodes_length,1)*OpenSet(min_idx,1),NewNodes));
+    
+    % dodanie informacji o d³ugoœci œcie¿ki powiêkszon¹ o funkcjê
+    % heurystyczn¹
+    OpenSet(end-NewNodes_length+1:end,3) = OpenSet(end-NewNodes_length+1:end,2)+h(NewNodes);
+    
+    % przepisanie indeksów poprzednich wêz³ów w œcie¿kach
+    OpenSet(end-NewNodes_length+1:end,4) = ones(NewNodes_length,1)*OpenSet(min_idx,1);
+    OpenSet(end-NewNodes_length+1:end,5:end) = ones(NewNodes_length,length(OpenSet(1,:))-4).*OpenSet(min_idx,4:end-1);
+    
+    % wersja iteracyjna
+    %for i = 1:length(temp)
+    %{
+        OpenSet(end+1,1) = temp(i);
+        OpenSet(end,2) = OpenSet(min_idx,2) + G.Edges.Weight(findedge(G,OpenSet(min_idx,1),temp(i)));
+        OpenSet(end,3) = OpenSet(end,2)+h(temp(i));
+        % dodanie wêz³a poprzedzaj¹cego
+        OpenSet(end,4) = OpenSet(min_idx,1);
+        % przepisanie wêz³ów poprzedzaj¹cych wêze³ poprzedzaj¹cy
+        OpenSet(end,5:end) = OpenSet(min_idx,4:end-1);
+    %}
+    %end
+    
+    % usuniêcie wêz³a, który by³ rozwijany
+    OpenSet(min_idx,:) = [];
+    
+    % wybranie wêz³a o minimalnej wartoœci funkcji
+    [~, min_idx] = min(OpenSet(:,3));
+    
+    % sprawdzenie czy wêze³ minimalizuj¹cy fonkcjê jest wêz³em koñcowym
+    if OpenSet(min_idx,1) == goal_idx
         % jeœli to prawda, zwrócenie œcie¿ki i d³ugoœci œcie¿ki
-        TEST = OpenSet(1,OpenSet(1,:)~=0);
-        START = cellstr(start);
-        GOAL = cellstr(goal);
-        PATH = [GOAL, G.Nodes.Name(TEST(1,4:end))', START];
-        D = OpenSet(1,2);
+        TEST = OpenSet(min_idx,OpenSet(min_idx,:)~=0);
+        PATH = [cellstr(goal), G.Nodes.Name(TEST(1,4:end))', cellstr(start)];
+        D = OpenSet(min_idx,2);
         k
         break
     end
     
     % jeœli nie jest to wêze³ koñcowy wracamy na pocz¹tek pêtli
     
-    
-    
 end
-
-
-
-
-
-
-
 end
