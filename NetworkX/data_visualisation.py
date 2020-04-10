ea74976fa6d4fbf9c685528874359161cd77e460
Rafanik <rafalszczepanik1998@gmail.com>
2020-04-10 12:43:43 +0200

    Dodane wagi do krawędzi grafu.

--- NetworkX/data_visualisation.py
+++ NetworkX/data_visualisation.py
@@ -1,3 +1,4 @@
+import math
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
@@ -22,14 +23,33 @@ G=nx.Graph()
 G.add_nodes_from(M[:,0])
 nx.set_node_attributes(G,dict(zip(M[:,0],M[:,1])),'X')
 nx.set_node_attributes(G,dict(zip(M[:,0],M[:,2])),'Y')
-G.add_edges_from(D)
 
-# creating specific dictionary for visualisation
-pos = {}
+# preparing graph edges weights
+Z = np.zeros((len(D[:,1]),1))
+D = np.append(D,Z,axis=1)
 name = list(G.nodes)
 X = list(G.nodes('X'))
 Y = list(G.nodes('Y'))
+for data in D:
+    start = name.index(data[0])
+    end = name.index(data[1])
+    data[2] = math.sqrt((X[start][1]-X[end][1])**2+(Y[start][1]-Y[end][1])**2)
+
+# adding edges to graph
+G.add_weighted_edges_from(D)
+
+# creating specific dictionary for visualisation
+pos = {}
 for i in range(len(G.nodes)):
     pos[name[i]]=[X[i][1], Y[i][1]]
 nx.draw_networkx(G,pos=pos,node_size=100)
+
+# calculating shortest path
+path = nx.shortest_path(G,source='Gdansk',target='Warszawa')
+path_edges = set(zip(path,path[1:]))
+
+# highlighting shortest path on graph
+nx.draw_networkx_nodes(G,pos=pos,nodelist=path,node_color='r',node_size=100)
+nx.draw_networkx_edges(G,pos,edgelist=path_edges,edge_color='r',width=2)
+
 plt.show()
