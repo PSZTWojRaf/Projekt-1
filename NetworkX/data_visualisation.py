79b66df2cb21e1f5696b3474d0cc58910b261ce0
Rafanik <rafalszczepanik1998@gmail.com>
2020-04-10 15:29:00 +0200

    Implementacja A*.

--- NetworkX/data_visualisation.py
+++ NetworkX/data_visualisation.py
@@ -1,9 +1,79 @@
 import math
+import copy
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 import xlrd
 import pandas as pd
+
+# heuristics computation
+def h(start, end):
+    start_idx = name.index(start)
+    end_idx = name.index(end)
+    return math.sqrt((X[start_idx][1]-X[end_idx][1])**2+(Y[start_idx][1]-Y[end_idx][1])**2)
+
+# A* algorithm
+def Astar(start, end):
+
+    class State:
+        def __init__(self, name, path, cost):
+            # city name
+            self.name = name
+            # case for init node
+            if(len(path))==0:
+                self.predecessor = name
+            else:
+                self.predecessor = path[-1]
+            # heuristic function
+            self.h = h(self.name, end)
+            # cost of path until this city
+            self.cost = cost+h(self.name, self.predecessor)
+            # cost + h - used to sort
+            self.h_and_cost = self.h + self.cost
+            # it needs to be copied
+            self.path = copy.copy(path)
+            self.path.append(name)
+
+        # all below needed to make comparisions between objects
+        def __lt__(self, other):
+            return  self.h_and_cost < other.h_and_cost
+
+        def __le__(self, other):
+            return  self.h_and_cost <= other.h_and_cost
+
+        def __eq__(self, other):
+            return  self.h_and_cost == other.h_and_cost
+
+        def __ge__(self, other):
+            return  self.h_and_cost >= other.h_and_cost
+
+        def __gt__(self, other):
+            return  self.h_and_cost > other.h_and_cost
+
+        def __ne__(self, other):
+            return  self.h_and_cost != other.h_and_cost
+
+        def __str__(self):
+            return "name: " +self.name + ", path: " + str(self.path)+", cost: "+str(self.cost)+", h(s):"+str(self.h)
+
+    # initial state
+    S1 = [State(start, [], 0)]
+    #print(str(current))
+    current = S1[0]
+    while current.name != end:
+        # searching for the best next-state
+        current = min(S1)
+        # adding new states
+        for s in list(G.neighbors(current.name)):
+            # checking if not going back
+            if current.predecessor != s:
+                new = State(s, current.path, current.cost)
+                S1.append(new)
+        # removing the old state
+        S1.remove(current)
+    # debug
+    print(str(current))
+    return current.path
 
 # loading data from excel using pandas
 # r - to produce raw string otherwise problems with "\"
@@ -31,9 +101,9 @@ name = list(G.nodes)
 X = list(G.nodes('X'))
 Y = list(G.nodes('Y'))
 for data in D:
-    start = name.index(data[0])
-    end = name.index(data[1])
-    data[2] = math.sqrt((X[start][1]-X[end][1])**2+(Y[start][1]-Y[end][1])**2)
+    start_idx = name.index(data[0])
+    end_idx = name.index(data[1])
+    data[2] = math.sqrt((X[start_idx][1]-X[end_idx][1])**2+(Y[start_idx][1]-Y[end_idx][1])**2)
 
 # adding edges to graph
 G.add_weighted_edges_from(D)
@@ -44,12 +114,21 @@ for i in range(len(G.nodes)):
     pos[name[i]]=[X[i][1], Y[i][1]]
 nx.draw_networkx(G,pos=pos,node_size=100)
 
-# calculating shortest path
+# calculating shortest path built-in alg.
 path = nx.shortest_path(G,source='Gdansk',target='Warszawa')
 path_edges = set(zip(path,path[1:]))
 
 # highlighting shortest path on graph
 nx.draw_networkx_nodes(G,pos=pos,nodelist=path,node_color='r',node_size=100)
 nx.draw_networkx_edges(G,pos,edgelist=path_edges,edge_color='r',width=2)
+
+# calculating shortest path custom alg.
+my_path = Astar('Szczecin','Wroclaw')
+my_path_edges = set(zip(my_path,my_path[1:]))
+
+# highlighting shortest path on graph
+nx.draw_networkx_nodes(G,pos=pos,nodelist=my_path,node_color='g',node_size=100)
+nx.draw_networkx_edges(G,pos,edgelist=my_path_edges,edge_color='g',width=2)
+
 
 plt.show()
